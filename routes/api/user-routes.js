1e46eec2a97847811e0b25f13084ada50e29e11d
Solidshots <seanbrent5@gmail.com>
2021-05-10 08:39:24 -0400

    Get all users route

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -2,7 +2,14 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 //Get /api/users
-router.get('/', (req, res) => {});
+router.get('/', (req, res) => {
+    User.findAll()
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(error);
+        res.status(500).json(err);
+    });
+});
 
 //Get /api/users/1
 router.get('/:id', (req, res) => {});
