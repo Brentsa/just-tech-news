b8291d9512aaf2c4a02d94adaa8c92500dc119af
Solidshots <seanbrent5@gmail.com>
2021-05-10 08:34:54 -0400

    user route definitions added

---
+++ routes/api/user-routes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+//Get /api/users
+router.get('/', (req, res) => {});
+
+//Get /api/users/1
+router.get('/:id', (req, res) => {});
+
+//Post /api/users
+router.get('/', (req, res) => {});
+
+//Put /api/users/1
+router.get('/:id', (req, res) => {});
+
+//Delete /api/users/1
+router.get('/:id', (req, res) => {});
+
+module.exports = router;
