3abf7393be61d845b29ffc2ce0a3c1cab4f963e9
Solidshots <seanbrent5@gmail.com>
2021-05-19 09:03:10 -0400

    add comment

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -13,16 +13,25 @@ router.get('/', (req, res) => {
 
 //Create a new comment
 router.post('/', (req, res) => {
-    Comment.create({
-        comment_text: req.body.comment_text,
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
+    //Check for a session
+
+    console.log({comment_text: req.body.comment_text,
+        post_id: req.body.post_id,
+        user_id: req.session.user_id
     })
-    .then(dbCommentData => res.json(dbCommentData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    })
+
+    if(req.session){
+        Comment.create({
+            comment_text: req.body.comment_text,
+            post_id: req.body.post_id,
+            user_id: req.session.user_id
+        })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+    }
 });
 
 //Delete a comment 
